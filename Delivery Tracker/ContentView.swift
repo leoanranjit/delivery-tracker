e175ff790b3c818d530d5ab8fb7abf1f099f84cb
Leoan Ranjit <leoanranjit1991@gmail.com>
2025-09-16 18:55:59 +1000

    creating contentView

--- Delivery Tracker/ContentView.swift
+++ Delivery Tracker/ContentView.swift
@@ -6,14 +6,26 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Map()
+            VStack(spacing: 12) {
+                VStack(spacing: 6) {
+                    Text("Latitude: 24.123121")
+                    Text("Longitude: 24.123121")
+                    Text("Distance: \(10, specifier: "%.2f") m")
+                        .font(.headline)
+                        .foregroundColor(.blue)
+                }
+                .padding()
+                .background(.ultraThinMaterial)
+                .cornerRadius(12)
+                .shadow(radius: 4)
+            }
+            .padding()
         }
         .padding()
     }
