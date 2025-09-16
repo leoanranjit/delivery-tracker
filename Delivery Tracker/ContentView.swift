cb5481d1f139d56b2d5b4c94824469356a85879e
Leoan Ranjit <leoanranjit1991@gmail.com>
2025-09-16 18:19:55 +1000

    chore: initial project setup

---
+++ Delivery Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Delivery Tracker
+//
+//  Created by Leoan Ranjit on 9/16/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Delivery Tracker/Delivery_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Delivery_TrackerApp.swift
+//  Delivery Tracker
+//
+//  Created by Leoan Ranjit on 9/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Delivery_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
